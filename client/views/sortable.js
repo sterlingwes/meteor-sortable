a81cc1ca5c2d36aeea08aafc4e355cc522029afa
Wes Johnson <swj@wesquire.ca>
2013-10-03 01:07:33 +0000

    post jqueryui modularization

--- client/views/sortable.js
+++ client/views/sortable.js
@@ -14,37 +14,28 @@ Template.sortable.helpers({
     
 });
 
-Template.sortable.created = function() {
-    Meteor.Loader.loadCss('/lib/jquery-ui/jquery-ui-1.10.3.custom.min.css');
-    Meteor.Loader.loadJs('/lib/jquery-ui/jquery-ui-1.10.3.custom.min.js', function() {
-        Session.set('hasJqueryUI',true);
-    });
-};
-
 Template.sortable.rendered = function() {
     var that = this;
-    Session.whenTrue('hasJqueryUI', function() {
-        $('.sortable').sortable({
-            connectWith:    ".sortable",
-            cancel:         ".dontsort",
-            update: function(evt,ui) {
+    $('.sortable').sortable({
+        connectWith:    ".sortable",
+        cancel:         ".dontsort",
+        update: function(evt,ui) {
 
-                var list = $(evt.target);
-                if(!list.hasClass('ui-sortable'))   return;
-                var children = list.children(),
-                    count = children.length;
-                if(count>1 && list.has('li.dontsort'))
-                    list.find('.dontsort').remove();
-                else if(count==0)
-                    list.append('<li class="dontsort">Nothing to sort.</li>');
-                
-                var ids =_.map(children, function(child) {
-                        return $(child).data('id');
-                    });
-                
-                if(this.data && this.data.session)
-                    Session.set(this.data.session, ids);
-            }
-        });
+            var list = $(evt.target);
+            if(!list.hasClass('ui-sortable'))   return;
+            var children = list.children(),
+                count = children.length;
+            if(count>1 && list.has('li.dontsort'))
+                list.find('.dontsort').remove();
+            else if(count==0)
+                list.append('<li class="dontsort">Nothing to sort.</li>');
+            
+            var ids =_.map(children, function(child) {
+                    return $(child).data('id');
+                });
+            
+            if(this.data && this.data.session)
+                Session.set(this.data.session, ids);
+        }
     });
 };
--- package.js
+++ package.js
@@ -5,8 +5,7 @@ Package.describe({
 Package.on_use(function(api) {
     
     api.use('standard-app-packages');
-    api.use('external-file-loader');
-    api.use('session-extras');
+    api.use('jqueryui');
     
     api.add_files([
         'client/views/sortable.html',
